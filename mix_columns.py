45b369e58a468fc624acb3fde00b879b804e88b7
Max <maxruetti@gmail.com>
2024-05-03 15:09:27 +0200

    masked all help functions

--- mix_columns.py
+++ mix_columns.py
@@ -2,39 +2,40 @@ from polynom import Polynom
 from state import State
 
 
-def xor_bytes(a: bytes, b: bytes) -> bytes:
+def _xor_bytes(a: bytes, b: bytes) -> bytes:
     assert len(a) == len(b)
     return bytes(x ^ y for x, y in zip(a, b))
 
 
-def multiply_with_polynom(byte: bytes, polynom: Polynom) -> bytes:
+def _multiply_with_polynom(byte: bytes, polynom: Polynom) -> bytes:
     return bytes(Polynom(byte) * polynom % Polynom(b'\x01\x1b'))
 
 
-def multiply_with_matrix(columns: bytes, matrix: bytes) -> bytes:
+def _multiply_with_matrix(columns: bytes, matrix: bytes) -> bytes:
     matrix = [Polynom(bytes([b])) for b in matrix]
     new_column = []
     for _ in range(4):
-        temp = [multiply_with_polynom(bytes([columns[0]]), matrix[0]),
-                multiply_with_polynom(bytes([columns[1]]), matrix[1]),
-                multiply_with_polynom(bytes([columns[2]]), matrix[2]),
-                multiply_with_polynom(bytes([columns[3]]), matrix[3])]
-        new_column.append(xor_bytes(xor_bytes(xor_bytes(temp[0], temp[1]), temp[2]), temp[3]))
+        temp = [_multiply_with_polynom(bytes([columns[0]]), matrix[0]),
+                _multiply_with_polynom(bytes([columns[1]]), matrix[1]),
+                _multiply_with_polynom(bytes([columns[2]]), matrix[2]),
+                _multiply_with_polynom(bytes([columns[3]]), matrix[3])]
+        new_column.append(_xor_bytes(_xor_bytes(_xor_bytes(temp[0], temp[1]), temp[2]), temp[3]))
         matrix = [matrix[-1]] + matrix[:-1]
     return bytes().join(new_column)
 
 
 def mix_columns(state: State) -> State:
     matrix = b'\x02\x03\x01\x01'
-    state.set_columns(state.map_columns(lambda column: multiply_with_matrix(column, matrix)))
+    state.set_columns(state.map_columns(lambda column: _multiply_with_matrix(column, matrix)))
     return state
 
 
 def inv_mix_columns(state: State) -> State:
     matrix = b'\x0e\x0b\x0d\x09'
-    state.set_columns(state.map_columns(lambda column: multiply_with_matrix(column, matrix)))
+    state.set_columns(state.map_columns(lambda column: _multiply_with_matrix(column, matrix)))
     return state
 
 
-print(State(b'0123465789ABCDEF'))
-print(inv_mix_columns(mix_columns(State(b'0123465789ABCDEF'))))
+if __name__ == '__main__':
+    print(State(b'0123465789ABCDEF'))
+    print(inv_mix_columns(mix_columns(State(b'0123465789ABCDEF'))))
--- sub_bytes.py
+++ sub_bytes.py
@@ -2,14 +2,14 @@ from polynom import Polynom
 from state import State
 
 
-def multiplicative_inverse(a: bytes) -> bytes:
+def _multiplicative_inverse(a: bytes) -> bytes:
     assert len(a) == 1
     if a == b'\x00': return a
     inverse = pow(Polynom(a), 254, Polynom(b'\x01\x1b'))
     return bytes(inverse)
 
 
-def affine_transformation(a: bytes) -> bytes:
+def _affine_transformation(a: bytes) -> bytes:
     new = ''
     bits = list(map(int, ''.join([bin(i)[2:].zfill(8) for i in a])))[::-1]
     c = [0, 1, 1, 0, 0, 0, 1, 1][::-1]
@@ -65,7 +65,7 @@ def inv_sub_bytes(state: State) -> State:
     return State(state.map_bytes(lambda byte: inv_sBox[byte]))
 
 
-def test_forward():
+def _test_forward():
     for i in range(256):
         b = bytes([i])
-        assert bytes([sBox[i]]) == affine_transformation(multiplicative_inverse(b))
+        assert bytes([sBox[i]]) == _affine_transformation(_multiplicative_inverse(b))
