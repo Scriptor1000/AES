5209f79b10c675a88c5d37dfb3c20e924654a23d
Max <maxruetti@gmail.com>
2024-05-06 18:02:40 +0200

    last improvement

--- block_cipher_modes.py
+++ block_cipher_modes.py
@@ -7,24 +7,15 @@ IV_LENGTH = 16
 KEY_LENGTHS = [16, 24, 32]
 
 
-def _encrypt_block(block, key):
-    schedule = KeySchedule(key)
-    return cipher(block, schedule.round_count, schedule)
-
-
-def _decrypt_block(block, key):
-    schedule = KeySchedule(key)
-    return inv_cipher(block, schedule.round_count, schedule)
-
-
 def cipher_block_chaining_encrypt(plaintext: bytes, key: bytes, iv: bytes) -> bytes:
     assert len(key) in KEY_LENGTHS
     assert len(iv) == IV_LENGTH
+    schedule = KeySchedule(key)
     plaintext = add_padding(plaintext, BLOCK_LENGTH)  # 16 bytes = 128 bits block size
     ciphertext = b''
     for i in range(0, len(plaintext), BLOCK_LENGTH):
         block = xor_bytes(bytes(plaintext[i:i + BLOCK_LENGTH]), iv)
-        block = _encrypt_block(block, key)
+        block = cipher(block, schedule.round_count, schedule)
         ciphertext += block
         iv = block
     return ciphertext
@@ -34,9 +25,10 @@ def cipher_block_chaining_decrypt(ciphertext: bytes, key: bytes, iv: bytes) -> b
     assert len(key) in KEY_LENGTHS
     assert len(iv) == IV_LENGTH
     assert len(ciphertext) % BLOCK_LENGTH == 0
+    schedule = KeySchedule(key)
     plaintext = b''
     for i in range(0, len(ciphertext), BLOCK_LENGTH):
-        block = _decrypt_block(ciphertext[i:i + BLOCK_LENGTH], key)
+        block = inv_cipher(ciphertext[i:i + BLOCK_LENGTH], schedule.round_count, schedule)
         block = xor_bytes(block, iv)
         plaintext += block
         iv = ciphertext[i:i + 16]
@@ -46,10 +38,11 @@ def cipher_block_chaining_decrypt(ciphertext: bytes, key: bytes, iv: bytes) -> b
 def cipher_feedback_encrypt(plaintext: bytes, key: bytes, iv: bytes) -> bytes:
     assert len(key) in KEY_LENGTHS
     assert len(iv) == IV_LENGTH
+    schedule = KeySchedule(key)
     plaintext = add_padding(plaintext, BLOCK_LENGTH)  # 16 bytes = 128 bits block size
     ciphertext = b''
     for i in range(0, len(plaintext), BLOCK_LENGTH):
-        block = _encrypt_block(iv, key)
+        block = cipher(iv, schedule.round_count, schedule)
         iv = xor_bytes(bytes(plaintext[i:i + BLOCK_LENGTH]), block)
         ciphertext += iv
     return ciphertext
@@ -59,9 +52,10 @@ def cipher_feedback_decrypt(ciphertext: bytes, key: bytes, iv: bytes) -> bytes:
     assert len(key) in KEY_LENGTHS
     assert len(iv) == IV_LENGTH
     assert len(ciphertext) % BLOCK_LENGTH == 0
+    schedule = KeySchedule(key)
     plaintext = b''
     for i in range(0, len(ciphertext), BLOCK_LENGTH):
-        block = _encrypt_block(iv, key)
+        block = cipher(iv, schedule.round_count, schedule)
         iv = ciphertext[i:i + BLOCK_LENGTH]
         plaintext += xor_bytes(iv, block)
     return remove_padding(plaintext)
@@ -94,9 +88,10 @@ def counter_decrypt(ciphertext: bytes, key: bytes, iv: bytes) -> bytes:
 
 
 def _output_feedback(message: bytes, key: bytes, iv: bytes) -> bytes:
+    schedule = KeySchedule(key)
     result = b''
     for i in range(0, len(message), BLOCK_LENGTH):
-        block = _encrypt_block(iv, key)
+        block = cipher(iv, schedule.round_count, schedule)
         result += xor_bytes(bytes(message[i:i + BLOCK_LENGTH]), block)
         iv = block
     return result
@@ -105,9 +100,10 @@ def _output_feedback(message: bytes, key: bytes, iv: bytes) -> bytes:
 def _counter(message: bytes, key: bytes, iv: bytes) -> bytes:
     assert len(iv) == IV_LENGTH
     assert len(message) / BLOCK_LENGTH < 2 ** 32
+    schedule = KeySchedule(key)
     result = b''
     for i in range(0, len(message), BLOCK_LENGTH):
-        block = _encrypt_block(iv, key)
+        block = cipher(iv, schedule.round_count, schedule)
         result += xor_bytes(bytes(message[i:i + BLOCK_LENGTH]), block)
         iv = _increment(iv)
     return result
